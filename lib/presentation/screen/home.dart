6808e51c62b3ecb779d8256f8876b8030d51711c
shimaa2000 <shimaam5256@gmail.com>
2023-04-28 01:49:46 +0300

    lints utils

--- lib/presentation/screen/home.dart
+++ lib/presentation/screen/home.dart
@@ -30,8 +30,10 @@ class Home extends StatelessWidget {
                 style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              )),
+                    color: Colors.white,
+                    ),
+              ),
+              ),
             ),
           );
         },
